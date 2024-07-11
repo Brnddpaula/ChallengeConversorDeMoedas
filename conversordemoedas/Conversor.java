34bca0035abce5124ee0a4040c9f8a3888a1f7e2
Brenda de Paula <brnd.dpaula@gmail.com>
2024-07-10 21:59:29 -0300

    Implementação da classe Conversor

--- conversordemoedas/Conversor.java
+++ conversordemoedas/Conversor.java
@@ -1,4 +1,8 @@
 package conversordemoedas;
 
 public class Conversor {
+    // Método para converter um valor de uma moeda para outra
+    public double converter(double valor, double taxaOrigem, double taxaDestino) {
+        return valor * (taxaDestino / taxaOrigem);
+    }
 }
